2620aefa233075d73f418a94349f816636a131da
Jeshu Brij <jeshu911@gmail.com>
2021-03-12 23:48:58 +0530

    Dice app done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,27 +16,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
-  // const dicePressed  => () {}
+class DicePage extends StatefulWidget {
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void randomState () {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) +1;
+      rightDiceNumber = Random().nextInt(6) +1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
-      child:Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-                child: Image.asset('images/dice1.png'),
-                // onPressed: dicePressed(),
+        child:Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                onPressed: randomState,
+              ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            Expanded(
+              child: FlatButton(
+                onPressed: randomState,
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
             ),
-          ),
-        ],
-      )
+          ],
+        )
     );
   }
 }
